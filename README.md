e5c0202cc980e93fdb016d2fb9f6dd7006f5d429
Jezeniel Zapanta <jpzapanta22@gmail.com>
2013-01-30 15:18:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+##Student Portal CMS
+========
+
+A simple platform for creating student portals written in django
+
---
+++ student_portal_platform/student_portal_platform/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'student_portal_platform.views.home', name='home'),
+    # url(r'^student_portal_platform/', include('student_portal_platform.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
