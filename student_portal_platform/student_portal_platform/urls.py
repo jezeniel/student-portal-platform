8ed591f2de7a684790e58bfcf2133053c423001b
Jezeniel Zapanta <jpzapanta22@gmail.com>
2013-01-31 00:48:42 +0800

    configured urls in urls.py -added admin, login, logout, home url

--- student_portal_platform/student_portal_platform/urls.py
+++ student_portal_platform/student_portal_platform/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,8 +9,13 @@ urlpatterns = patterns('',
     # url(r'^student_portal_platform/', include('student_portal_platform.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^$', 'login.views.login_view', name = 'login_url'),
+    url(r'^home/' , 'login.views.home_view', name = 'home_url'),
+    url(r'^logout/', 'login.views.logout_view', name= 'logout_url')
+    
 )
