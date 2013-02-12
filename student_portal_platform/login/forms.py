9b1ebb4a101ff01c6a8d9a2c76ab8deba4e8ee26
Jezeniel Zapanta <jpzapanta22@gmail.com>
2013-02-13 01:41:26 +0800

    add remember field

--- student_portal_platform/login/forms.py
+++ student_portal_platform/login/forms.py
@@ -4,3 +4,4 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget = forms.PasswordInput)
+    remember = forms.BooleanField(required=False,label=(u"Remember Me?"))
