5299c32c815bd4f7e73a0ca68a1d5e50d18fd575
Jezeniel Zapanta <jpzapanta22@gmail.com>
2013-02-13 01:52:15 +0800

    remove home_view and add remember when logging in -remember has two weeks duration

--- student_portal_platform/login/views.py
+++ student_portal_platform/login/views.py
@@ -6,27 +6,25 @@ from django.contrib import messages
 
 from .forms import LoginForm
 
-@login_required(login_url="login_url")
-def home_view(request):
-    return render(request, "home.html", { 'user': request.user })
-    
 def login_view(request):
-    
     if request.user.is_authenticated():
         return redirect("home_url")
     #if method is GET render login page else authenticate user
     if request.method.upper() == "GET":
         #if user is already logged in go to home page
-        return render(request, 'login/login.html', {'loginform' : LoginForm()})
+        return render(request, 'development/login.html', 
+                      {'loginform' : LoginForm()})
     
     elif request.method.upper() == "POST":
         form = LoginForm(request.POST)
         #check if the fields are valid
         if form.is_valid():
             #authenticates user return not if doesn't exist
-            user = authenticate(username = form.cleaned_data['username'], password = form.cleaned_data['password'])
+            user = authenticate(username = form.cleaned_data['username'], 
+                                password = form.cleaned_data['password'])
             if user:
-                request.session.set_expiry(0)
+                if request.POST.has_key("remember"):
+                    request.session.set_expiry(1209600)
                 login(request, user)
                 return redirect('home_url')
         messages.error(request, "Invalid Login!")
