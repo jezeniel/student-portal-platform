c93092ae7a3daadd64be8b944ac9f498852f1530
Jezeniel Zapanta <jpzapanta22@gmail.com>
2013-02-24 04:51:32 +0800

    Add next hidden field and redirect in login form

--- student_portal_platform/login/forms.py
+++ student_portal_platform/login/forms.py
@@ -4,4 +4,6 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget = forms.PasswordInput)
-    remember = forms.BooleanField(required=False,label=(u"Remember Me?"))
+    remember = forms.BooleanField(required = False, label=(u"Remember Me?"))
+    next = forms.CharField(widget = forms.HiddenInput(), 
+                           label = '', required = False)
--- student_portal_platform/login/views.py
+++ student_portal_platform/login/views.py
@@ -7,30 +7,48 @@ from django.contrib import messages
 from .forms import LoginForm
 from announcement.models import Announcement
 
+SESSION_TIME = 1209600 #two weeks
+BROWSER_CLOSE = 0
+
 def login_view(request):
+    #if user is already logged in go to home page
     if request.user.is_authenticated():
-        #if user is already logged in go to home page
         return redirect("home_url")
+    
     #if method is GET render login page else authenticate user
     if request.method.upper() == "GET":
-        x = Announcement.objects.order_by("-postdate")[0]
+        loginform = LoginForm()
+        #get the next link the have an initial value for it in the login formTWO_WEEKS
+        if request.GET.has_key('next'):
+            loginform.initial['next'] = request.GET.get("next")
+        #get the latest announcement by postdate
+        announcement = Announcement.objects.order_by("-postdate")[0]
         return render(request, 'proto_design/index.html', 
-                      {'loginform' : LoginForm(), 'announcement' : x})
-    
+                      {'loginform' : loginform, 'announcement' : announcement})
+        
     elif request.method.upper() == "POST":
         form = LoginForm(request.POST)
         #check if the fields are valid
         if form.is_valid():
-            #authenticates user return not if doesn't exist
+            #authenticates user return None if doesn't exist
             user = authenticate(username = form.cleaned_data['username'], 
                                 password = form.cleaned_data['password'])
             if user:
+                # if the remember checkbox is checked the session will expire after
+                # two weeks else it will end the session will expire when browser is closed
                 if request.POST.has_key("remember"):
-                    request.session.set_expiry(1209600)
+                    request.session.set_expiry(SESSION_TIME)
                 else:
-                    request.session.set_expiry(0)
+                    request.session.set_expiry(BROWSER_CLOSE)
+                
                 login(request, user)
-                return redirect('home_url')
+                # if there is next it will redirect to the link
+                # else just go to the homepage
+                if request.POST.get("next"):
+                    return redirect(request.POST['next'])
+                else:
+                    return redirect('home_url')
+            
         messages.error(request, "Invalid Login!")
         return redirect('login_url')
 
