d3f7651e43ccbe56a2c6c730bf1174d321735c34
Jezeniel Zapanta <jpzapanta22@gmail.com>
2013-03-06 04:06:50 +0800

    Change Announcement into abstract class -Make GlobalAnnouncement and SubjectAnnouncement  from Announcement class

--- student_portal_platform/announcement/models.py
+++ student_portal_platform/announcement/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from subject.models import Subject
+
 class Announcement(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User)
     content = models.TextField()
     postdate = models.DateTimeField(auto_now_add = True)
-    subjectcode = models.CharField(max_length = 50, default = 'global')
+    
+    class Meta:
+        abstract = True
+
+
+class SubjectAnnouncement(Announcement):
+    subject = models.ForeignKey(Subject)
     
     def __unicode__(self):
-        return self.subjectcode + '--' + self.title + '-' + str(self.postdate)
+        return "%s : %s - %s" % (self.subject, self.title, str(self.postdate))
+        
+class GlobalAnnouncement(Announcement):
+    def __unicode__(self):
+        return "%s - %s"  % (self.title, str(self.postdate))
+
+    
     
