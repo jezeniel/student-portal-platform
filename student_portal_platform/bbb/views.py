29c1c6d11cfc75ba81fc99e27c598c7df3df6be3
Jezeniel Zapanta <jpzapanta22@gmail.com>
2013-04-03 05:42:34 +0800

    add error message when joining rooms

--- student_portal_platform/bbb/views.py
+++ student_portal_platform/bbb/views.py
@@ -12,14 +12,20 @@ from subject.models import Subject
 def create_room(request, course_id):
     subject = get_object_or_404(Subject, id=course_id)
     result = subject.meeting.create(HttpRequest.build_absolute_uri(request, subject.get_absolute_url()))
-    messages.success(request, "Successfully created a room!")
+    if result is not None:
+        messages.success(request, "Successfully created a conference.")
+    else:
+        messages.error(request, "Room already created or not available.")
     return redirect(subject.get_absolute_url())
 
 
 def end_room(request, course_id):
     subject = get_object_or_404(Subject, id=course_id)
     result = subject.meeting.end()
-    messages.success(request, "Successfully ended a room!")
+    if result is not None:
+        messages.success(request, "Successfully ended the current conference.")
+    else:
+        messages.error(request, "Failed to end conference. Maybe you did not start any conference?")
     return redirect(subject.get_absolute_url())
 
 
@@ -27,7 +33,7 @@ def join_room(request, course_id):
     subject = get_object_or_404(Subject, id=course_id)
     url, code = subject.meeting.join(request.user.get_full_name(), request.user.groups.all()[0].name)
     if code == "FAILED":
-        messages.error(request, "There are no class currently ongoing!")
+        messages.error(request, "There are no class currently ongoing.")
         return redirect(subject.get_absolute_url())
     else:
         return redirect(url)
